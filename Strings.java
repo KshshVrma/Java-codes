14cc0c51cba91f3fb134917681f300f959a6303c
Kashish <kashishverma8382@gmail.com>
2023-12-30 11:19:28 +0530

    .

---
+++ Strings.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String t = sc.nextLine();
+        String[] temp = t.split(" ");
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println(temp[i]);
+        }
+
+    }
+}
