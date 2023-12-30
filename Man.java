12d0bcb8340a327ddd676d3ec2a51d05e8af5b6c
Kashish <kashishverma8382@gmail.com>
2023-12-30 11:57:21 +0530

    .

---
+++ Man.java
@@ -0,0 +1,8 @@
+import example.MathUtils;
+
+public class Man {
+    public static void main(String[] args) {
+        int result = MathUtils.add(5, 3);
+        System.out.println(result); // Output: 8
+    }
+}
